28f3fdfea97273741e8a6caefc3e92ca741aca65
Dhanashri835 <dhanudahake186@gmail.com>
2023-11-02 16:57:28 +0530

    third commite

--- DOM.js
+++ DOM.js
@@ -19,7 +19,7 @@
 //console.log(document.getElementById('header-title'));
 //var headerTitle = document.getElementById('header-title');
 //var header = document.getElementById('main-title');
-console.log(headerTitle);
+//console.log(headerTitle);
 //headerTitle.textContent = 'Hello';
 //headerTitle.innerText = 'Goodbye';
 //console.log(headerTitle.innerText);
@@ -27,11 +27,17 @@ console.log(headerTitle);
 //header.style.borderBottom = 'solid 3px #000';
 
 //Get element by class //
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[2].style.backgroundColour = 'green';
+var li = document.getElementsByClassName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColour = 'yello';
  
-for(var i=0; i<items.length; i++){
-    items[i].style.fontWeight = 'bold';
+for(var i=0; i<li.length; i++){
+    li[i].style.backgroundColour = '#f4f4d4';
 }
+
+//Queryselector //
+var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
