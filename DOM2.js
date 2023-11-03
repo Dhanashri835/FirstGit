5cc8d54cc3cef59ac5dc05bfe413a23f99a8f0e2
Dhanashri835 <dhanudahake186@gmail.com>
2023-11-03 15:43:58 +0530

    sixth commite

--- DOM2.js
+++ DOM2.js
@@ -1,4 +1,20 @@
+// Traversing the DOM //
+//var itemList = document.querySelector('#items');
 
+// ParentNode //
+//console.log(itemList.parentNode);
+//itemList.parentNode.style.backgroundColor = '#f4f4f4';
+//console.log(itemList.parentNode.parentNode);
+
+// ParentElement //
+//console.log(itemList.parentElement);
+//itemList.parentElement.style.backgroundColor = '#f4f4f4';
+//console.log(itemList.parentElement.parentElement);
+
+// ChildNode //
+//console.log(itemList.childNodes);
+//console.log(itemList.children);
+//console.log(itemList.children[1]);
 
 // firstChild //
 console.log(itemList.firstChild);
@@ -8,11 +24,20 @@ itemList.firstElementChild.textContent = 'Hello world Item 1';
 
 // Create a div //
 var newDiv = document.createElement('div');
+
 // add class
 newDiv.className = 'Hello';
+
+//add id //
+newDiv.id = 'hello 1';
+
+// add attr //
+newDiv.setAttribute('title', 'Hello div');
+
 //create text node
 var newDivText = document.createTextNode('hello World');
-//add text 
+
+//add text dom
 var container = document.querySelector('header.container');
 var h1 = document.querySelector('header h1');
 
