3fff4ee19c5e11b6c63e30328c4567e49e4b0e98
Dhanashri835 <dhanudahake186@gmail.com>
2023-11-05 15:04:09 +0530

    8th commite

--- DOM3.js
+++ DOM3.js
@@ -6,6 +6,8 @@ var filter = document.getElementById('filter');
 form.addEventListener('submit', addItem);
 // Delete event
 itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
 
 // Add item
 function addItem(e){
@@ -66,3 +68,21 @@ function removeItem(e){
     }
   }
 }
+
+// Filter Items
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
+  
